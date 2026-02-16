3e97264dd4a75bd26892632bff7c6b27c75c4862
iamalessiastocco <alessiastocco01@gmail.com>
2026-02-16 10:34:53 +0100

    modifica main.ts

--- smart-billing-tool/src/router/index.ts
+++ smart-billing-tool/src/router/index.ts
@@ -1,8 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+
+import companySearch from '../views/companySearch.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'CompanySearch',
+      component: companySearch
+    }
+  ],
 })
 
 export default router
